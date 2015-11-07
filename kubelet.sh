06efaec010720da74311ecf9b9e98a96783ad383
Chris Baumbauer <cab@cabnetworks.net>
2015-11-06 17:36:15 -0800

    Cleaned up optional cert check

--- kubelet.sh
+++ kubelet.sh
@@ -2,12 +2,7 @@
 
 run_start() {
 	echo "Starting kubelet..."
-	if [ -f $KUBE_CERT ] && [ -f $KUBE_KEY ]; then 
 		/usr/local/bin/kubelet --api-servers=http://127.0.0.1:8080 --allow-privileged=true --config=/etc/kubernetes/manifests --kubeconfig=/etc/kubernetes/kubelet.kubeconfig --v=2 > /var/log/kubelet.log 2>&1 &
-	else
-		/usr/local/bin/kubelet --api-servers=http://127.0.0.1:8080 --allow-privileged=true --config=/etc/kubernetes/manifests --v=2 > /var/log/kubelet.log 2>&1 &
-	fi
-
 }
 
 run_stop() {
